115f1295b96d572149c2c7680b2ee68295e04dc4
Robert <rsuazo@doterra.com>
2026-09-01 12:38:25 -0600

    Change var to CONST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  let environmentVar = process.env.NEXT_PUBLIC_SPIKE_ENV;
+  const environmentVar = process.env.NEXT_PUBLIC_SPIKE_ENV;
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
